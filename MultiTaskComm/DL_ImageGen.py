3175e23860a549589caa8934d406810b501f2dd2
Joy, Shin <hkshin2@gmail.com>
2017-01-17 09:17:48 +0900

    Add JSON Configuration
    Add JSON Configuration

--- MultiTaskComm/DL_ImageGen.py
+++ MultiTaskComm/DL_ImageGen.py
@@ -3,21 +3,40 @@
 
 import os
 import time
+import json
 
-def DirFileCount(path_name, buff):
+RUN_Status = {
+    'MaxCount': 10,
+    'CurrentCount': 1,
+    'FileName': ''
+}
 
+
+def DirFileCount(msg, buff):
+    path_name = msg['file_path']
     tot_files = 0
 
     for dir_path in os.listdir(path_name):
         sub_dir = path_name + '/' + dir_path
 
-        files = len(os.listdir(sub_dir))
-        data = buff.put(tot_files)
+        sub_files = os.listdir(sub_dir)
+
+        files = len(sub_files)
+
+        RUN_Status['MaxCount'] = files
+
+        cnt = 0;
+        for file in sub_files:
+            cnt = cnt + 1
+            RUN_Status['CurrentCount'] = cnt
+            RUN_Status['FileName'] = file
+            data = buff.put(RUN_Status)
+            time.sleep(0.01)
 
         tot_files = tot_files + files
-        print dir_path, ' - ', files
+        #print dir_path, ' - ', files
 
-    print 'Total Files : ', tot_files
+    #print 'Total Files : ', tot_files
 
 # Main Startup....
 # *****************************************************************************
